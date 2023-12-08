3712be3e1557e7789eb85f50fe640ceff916721b
Yasser Busati <59507111+YasserBusati@users.noreply.github.com>
2023-12-08 13:25:10 +0300

    initial commit to build E-commerce app > setup .Net & React

---
+++ Client/src/App.tsx
@@ -0,0 +1,13 @@
+
+  const App = () => {
+    return (
+      <>
+      
+      <div className="text-blue-500 flex mt-5 p-5 text-5xl justify-center">App</div>
+    
+      </>
+
+    )
+  }
+  
+  export default App

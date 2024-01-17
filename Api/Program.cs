e02285882acf0aaa441a2554b137dfaff6ac66a9
Yasser Busati <59507111+YasserBusati@users.noreply.github.com>
2024-01-17 08:28:51 +0300

    add product entity and config DbContext and create endpoint for the product

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,15 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<StoreContext>(option =>
+    {
+        option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
 }
 var app = builder.Build();
 {
@@ -13,5 +20,20 @@ var app = builder.Build();
     }
     app.UseAuthorization();
     app.MapControllers();
+
+    var scop = app.Services.CreateScope();
+    var context = scop.ServiceProvider.GetRequiredService<StoreContext>();
+    var logger = scop.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+    try
+    {
+        context.Database.Migrate();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "A Problem occurred while migration");
+    }
+
     app.Run();
 }
